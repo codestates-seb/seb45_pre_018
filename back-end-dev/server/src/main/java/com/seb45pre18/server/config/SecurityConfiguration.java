24815cde8552d0ffc96d98f244bfebf2b791b288
hojun lee <ghwns123212@gmail.com>
2023-08-22 19:00:47 +0900

    add cors

---
+++ back-end-dev/server/src/main/java/com/seb45pre18/server/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.seb45pre18.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3006/")
+                .allowedMethods("POST")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
